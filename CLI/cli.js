d783049818414bc0b6e7c2326377c6fdee44a3fd
syaker <60866313+syaker@users.noreply.github.com>
2020-05-08 14:40:37 -0500

    Actualizacion CLI

--- CLI/cli.js
+++ CLI/cli.js
@@ -4,4 +4,3 @@ const { mdlinks } = require('../API/src/mdlinks');
 const [,, ...args] = process.argv
 
 console.log(process.argv[2])
-
--- test/mdlinks.spec.js
+++ test/mdlinks.spec.js
@@ -7,18 +7,21 @@ describe('Recibe, verifica y convierte la ruta', () => {
 	})
 
 	it('Tiene que retornar true para saber que es absoluta', () => {
-		expect(mdFunctions.verificateAbsolute("C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src")).toBe(true);
+		expect(mdFunctions.verificateAbsolute("API\\src\\mdlinks.js")).toBe('C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\mdlinks.js');
 	})
 
-	it('Tiene que devolver la ruta relativa convertida a absoluta', () => {
-		expect(mdFunctions.convertToAbsolute('API\\src\\archivesmd\\archivesmdvalid')).toBe("C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archivesmdvalid");
+	it('Tiene que retornar true para saber que es absoluta', () => {
+		expect(mdFunctions.verificateAbsolute("C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src")).toBe('C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src');
 	})
+
 })
 
 describe('Archivo o directorio, extrae .md, busca links, valida', () => {
 
 	it('Tiene que retornar un array con solo los archivos .md', () => {
-		expect(mdFunctions.extractMD(['C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\archive6.md'])).toEqual(['C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\archive6.md']);
+		mdFunctions.extractMD(['C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\archive6.md'])
+			.then((a) => expect(a).toEqual(['C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\archive6.md']))
+
 	})
 
 	it('Tiene que retornar un array con objetos que contengan info de las URL', () => {
@@ -36,8 +39,8 @@ describe('Archivo o directorio, extrae .md, busca links, valida', () => {
 		]);
 	})
 
-	it('Tiene que retornar un array de objetos que contengan las info con el status de las URL', () => {
-		expect(validateFunctions.validateLinks([
+	it('Tiene que retornar un array de objetos', () => {
+		validateFunctions.validateLinks([
 			{
 				href: 'https://nodejs.org/api/path.html',
 				text: 'https://nodejs.org/api/path.html',
@@ -48,46 +51,41 @@ describe('Archivo o directorio, extrae .md, busca links, valida', () => {
 				text: 'https://docs.google.com/spreadsheets/d/1Ka4UZFO20s',
 				link: 'C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md'
 			}
-		])).toEqual([
-			{
-				href: 'https://nodejs.org/api/path.html',
-				text: 'https://nodejs.org/api/path.html',
-				link: 'C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md',
-				status: 200,
-				statusText: 'OK'
-			},
-			{
-				href: 'https://docs.google.com/spreadsheets/d/1Ka4UZFO20syGtuveO99Wi49Lth6LiDhEdIGuew23fMk/edit#gid=443532081',
-				text: 'https://docs.google.com/spreadsheets/d/1Ka4UZFO20s',
-				link: 'C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md',
-				status: 200,
-				statusText: 'OK'
-			}
-		]);
-
-		it('Tiene que retornar un objeto que contiene informacion de la URL con status', () => {
-			expect(validateFunctions.promisesFetch(
-				{
-					href: 'https://nodejs.org/api/path.html',
-					text: 'https://nodejs.org/api/path.html',
-					link: 'C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md'
-				}
-			)).toBe(
+		])
+			.then((b) => expect(b).toEqual([
 				{
 					href: 'https://nodejs.org/api/path.html',
 					text: 'https://nodejs.org/api/path.html',
 					link: 'C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md',
 					status: 200,
 					statusText: 'OK'
-				  }
-			)
-		})
+				},
+				{
+					href: 'https://docs.google.com/spreadsheets/d/1Ka4UZFO20syGtuveO99Wi49Lth6LiDhEdIGuew23fMk/edit#gid=443532081',
+					text: 'https://docs.google.com/spreadsheets/d/1Ka4UZFO20s',
+					link: 'C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md',
+					status: 200,
+					statusText: 'OK'
+				}
+			]))
 
 	})
 
-	it('Tiene que retornar un array con todos las rutas de solo archivos'), () => {
-		expect(mdFunctions.archiveOrDirectory('C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\last')).toBe(['C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\last\\archive7.js']);
-	}
+	it('Tiene que retornar un objeto', () => {
+		validateFunctions.promisesFetch({ href: "https://www.facebook.com" })
+			.then((c) => expect(c).toStrictEqual( { href: 'https://www.facebook.com', status: 200, statusText: 'OK' }))
+
+	})
+
+	test('Tiene que retornar un array con todos las rutas de solo archivos', () => {
+		expect(mdFunctions.archiveOrDirectory('C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md')).toStrictEqual(['C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\archive1.md']);
+	});
+
+	test('Tiene que retornar un array con todos las rutas de solo archivos', () => {
+		expect(mdFunctions.archiveOrDirectory('C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\last')).toStrictEqual(
+			["C:\\Users\\cifer\\Desktop\\LIM012-fe-md-links\\API\\src\\archivesmd\\multiple\\last\\archive7.js"]);
+	});
+
 })
 
 describe('Todas deben ser funciones', () => {
@@ -97,10 +95,6 @@ describe('Todas deben ser funciones', () => {
 
 	it('verificateAbsolute deberia ser una funcion', () => {
 		expect(typeof mdFunctions.verificateAbsolute).toBe('function');
-	})
-
-	it('convertToAbsolute deberia ser una funcion', () => {
-		expect(typeof mdFunctions.convertToAbsolute).toBe('function');
 	})
 
 	it('archiveOrDirectory deberia ser una funcion', () => {
